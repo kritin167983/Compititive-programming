fecf6eb08308b6e4958a050693ee6cf1b046cc29
kritin <2023ucp1679@mnit.ac.in>
2024-10-24 20:20:21 +0530

    231A codeforce code

---
+++ 231A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ int t;
+ cin >> t;
+ int a[t],b[t],c[t];
+ int ans=0;
+ for(int i=0;i<t;i++)
+   {  cin >> a[i] >> b[i] >> c[i];
+      int p=0;
+      p=a[i]+b[i]+c[i];
+      if(p>1)
+        {ans++;}
+   }
+cout << ans ;
+
+}
+
